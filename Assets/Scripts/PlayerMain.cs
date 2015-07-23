25f89dd60d51dd7c25362c55027bdc7cf547733a
Chris Rice <chrisrice93@gmail.com>
2015-07-23 15:05:17 -0600

    added a resource counter script in player.

--- Assets/Scripts/PlayerMain.cs
+++ Assets/Scripts/PlayerMain.cs
@@ -16,8 +16,6 @@ public class PlayerMain : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.CompareTag("Killing Thing")) {
 			KillPlayer();
-		} else if (other.CompareTag("Tile")) {
-			Debug.Log("You are on a new tile: " + other.gameObject.name);
 		}
 	}
 
---
+++ Assets/Scripts/PlayerResources.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerResources : MonoBehaviour {
+
+	public Text tree_text;
+	public Text stone_text;
+	public Text water_text;
+
+	private int trees = 0;
+	private int stones = 0;
+	private int water = 0;
+
+	// Use this for initialization
+	void Start () {
+		UpdateResourcesText();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.CompareTag("Tree")) {
+			trees++;
+	   	} else if (collider.CompareTag("Stone")) {
+			stones++;
+	   	} else if (collider.CompareTag("Water")) {
+			water++;
+	   	}
+
+		UpdateResourcesText();
+	}
+
+	void UpdateResourcesText() {
+		tree_text.text = "Trees: " + trees.ToString();
+		stone_text.text = "Stones: " + stones.ToString();
+		water_text.text = "Water: " + water.ToString();
+	}
+}
